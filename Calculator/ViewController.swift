b6b9281c8a1d3ef9613fce7302b714786c055b32
Paul Kalupnieks <paul@unbounce.com>
2015-02-09 11:46:38 -0800

    Convert to tabs to match community guidelines

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,66 +10,73 @@ import UIKit
 
 class ViewController: UIViewController
 {
-    @IBOutlet weak var display: UILabel!
-    
-    var userIsTyping = false
-    
-    @IBAction func appendDigit(sender: UIButton) {
-        let digit = sender.currentTitle!
+	@IBOutlet weak var display: UILabel!
 
-        if (userIsTyping) {
-            display.text = display.text! + digit
-        } else {
-            display.text = digit
-            userIsTyping = true
-        }
-    }
-    
-    @IBAction func operate(sender: UIButton) {
-        let operation = sender.currentTitle!
-        if userIsTyping {
-            enter()
-        }
-        switch operation {
-        case "×": performOperation { $0 * $1 }
-        case "÷": performOperation { $1 / $0 }
-        case "+": performOperation { $0 + $1 }
-        case "−": performOperation { $1 - $0 }
-        case "√": performOperation { sqrt($0) }
-        default: break
-        }
-    }
-    
-    func performOperation(operation: (Double, Double) -> Double) {
-        if operandStack.count >= 2 {
-            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
-            enter()
-        }
-    }
-    
-    func performOperation(operation: Double -> Double) {
-        if operandStack.count >= 1 {
-            displayValue = operation(operandStack.removeLast())
-            enter()
-        }
-    }
-    
-    var operandStack = Array<Double>()
-    
-    @IBAction func enter() {
-        userIsTyping = false
-        operandStack.append(displayValue)
-        println("\(operandStack)")
-    }
-    
-    var displayValue: Double {
-        get{
-            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
-        }
-        set {
-            display.text = "\(newValue)"
-            userIsTyping = false
-        }
-    }
+	var userIsTyping = false
+
+	@IBAction func appendDigit(sender: UIButton) {
+		let digit = sender.currentTitle!
+
+		if (userIsTyping) {
+			display.text = display.text! + digit
+		} else {
+			display.text = digit
+			userIsTyping = true
+		}
+	}
+
+	@IBAction func operate(sender: UIButton) {
+		let operation = sender.currentTitle!
+
+		if userIsTyping {
+			enter()
+		}
+
+		switch operation {
+		case "×": performOperation { $0 * $1 }
+		case "÷": performOperation { $1 / $0 }
+		case "+": performOperation { $0 + $1 }
+		case "−": performOperation { $1 - $0 }
+		case "√": performOperation { sqrt($0) }
+		default: break
+		}
+	}
+
+	func performOperation(operation: (Double, Double) -> Double) {
+		if operandStack.count >= 2 {
+			displayValue = operation(
+				operandStack.removeLast(),
+				operandStack.removeLast()
+			)
+			enter()
+		}
+	}
+
+	func performOperation(operation: Double -> Double) {
+		if operandStack.count >= 1 {
+			displayValue = operation(operandStack.removeLast())
+			enter()
+		}
+	}
+
+	var operandStack = Array<Double>()
+
+	@IBAction func enter() {
+		userIsTyping = false
+		operandStack.append(displayValue)
+		println("\(operandStack)")
+	}
+
+	var displayValue: Double {
+		get{
+			return NSNumberFormatter()
+				.numberFromString(display.text!)!.doubleValue
+		}
+
+		set {
+			display.text = "\(newValue)"
+			userIsTyping = false
+		}
+	}
 }
 
